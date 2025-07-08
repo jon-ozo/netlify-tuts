f68cb8a40adb63654034ade888db3af0a5185d07
John <johnozoemena@gmail.com>
2025-07-08 14:08:19 +0100

    edit routes module - correct contact component path

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,3 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"), route('about', 'routes/about.tsx'), route('contact', 'contact.tsx'), route('services', 'routes/service.tsx')] satisfies RouteConfig;
+export default [index("routes/home.tsx"), route('about', 'routes/about.tsx'), route('contact', 'routes/contact.tsx'), route('services', 'routes/service.tsx')] satisfies RouteConfig;
